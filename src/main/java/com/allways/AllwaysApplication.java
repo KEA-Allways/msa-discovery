af36bbbd85e380be8b5d1f584d2821c919e7eed0
suha <hp980724@gmail.com>
2023-11-10 00:16:19 +0900

    ABF-95 feat : user cmd 추가

--- src/main/java/com/allways/AllwaysApplication.java
+++ src/main/java/com/allways/AllwaysApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//유레카 ok
 @EnableEurekaServer
 @SpringBootApplication
 public class AllwaysApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AllwaysApplication.class, args);
 	}
 
